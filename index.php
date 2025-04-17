c188296082bd90a3d681a7e528e771912d9567d9
KateLucena <53880309+KateLucena@users.noreply.github.com>
2025-04-16 21:29:15 -0300

    Add file in PHP
    Add index file in PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Portifólio</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Portifólio da Kate";
+        ?>
+    </h1>
+
+    <p> 
+        <?php 
+            echo "Kate";
+        ?>
+    </p>
+
+</body>
+</html>
